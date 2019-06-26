6609267ff245a1c431d81cebbd0f60db8885da80
JunRyung Ju <junryoungju@gmail.com>
2019-06-26 17:43:36 +0900

    Implements of flags to OS allocation flag transforms.

--- Source/Host/Memory.cpp
+++ Source/Host/Memory.cpp
@@ -1,14 +1,69 @@
 #include <Host/Memory.h>
 
 #define GTFW_PLATFORM_WINDOWS
-
-#define SET_IF_HAS(v, f, s) (((v & f) != 0) ? s : 0)
-
 #ifdef GTFW_PLATFORM_WINDOWS
 
 #include <Windows.h>
 namespace Goto
 {
+    unsigned int TransformHPF2MemFlags(unsigned int flags)
+    {
+        unsigned int osFlags = 0;
+        
+        if (flags & HPF_RESERVED)
+        {
+            osFlags |= MEM_RESERVE;
+        }
+
+        if (flags & HPF_COMMIT)
+        {
+            osFlags |= MEM_COMMIT;
+        }
+    }
+
+    unsigned int TransformHPF2PageFlags(unsigned int flags)
+    {
+        if (flags & HPF_NO_ACCESS)
+        {
+            return PAGE_NOACCESS;
+        }
+
+        if (flags & HPF_EXECUTABLE)
+        {
+            if (flags & HPF_READ && flags & HPF_WRITE)
+            {
+                return PAGE_EXECUTE_READWRITE;
+            }
+
+            if (flags & HPF_READ)
+            {
+                return PAGE_EXECUTE_READ;
+            }
+
+            if (flags & HPF_WRITE)
+            {
+                return PAGE_EXECUTE_WRITECOPY;
+            }
+        }
+
+        if (flags & HPF_READ && flags & HPF_WRITE)
+        {
+            return PAGE_READWRITE;
+        }
+
+        if (flags & HPF_READ)
+        {
+            return PAGE_READONLY;
+        }
+
+        if (flags & HPF_WRITE)
+        {
+            return PAGE_WRITECOPY;
+        }
+
+        return 0;
+    }
+
     void* AllocPage(size_t pages, unsigned int flags)
     {
         
