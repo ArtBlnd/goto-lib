7bd26738697d6692b45fcddfb09f967a1783dc70
JunRyung Ju <junryoungju@gmail.com>
2019-06-27 10:24:26 +0900

    Added missed namespace

--- Source/Host/Memory.cpp
+++ Source/Host/Memory.cpp
@@ -4,118 +4,122 @@
 #ifdef GTFW_PLATFORM_WINDOWS
 
 #include <Windows.h>
+#include <Windows.h>
 namespace Goto
-{
-    unsigned int TransformHPF2MemFlags(unsigned int flags)
+{   
+    namespace Host 
     {
-        unsigned int osFlags = 0;
-        
-        if (flags & HPF_RESERVED)
+        unsigned int TransformHPF2MemFlags(unsigned int flags)
         {
-            osFlags |= MEM_RESERVE;
+            unsigned int osFlags = 0;
+
+            if (flags & HPF_RESERVED)
+            {
+                osFlags |= MEM_RESERVE;
+            }
+
+            if (flags & HPF_COMMIT)
+            {
+                osFlags |= MEM_COMMIT;
+            }
         }
 
-        if (flags & HPF_COMMIT)
+        unsigned int TransformHPF2PageFlags(unsigned int flags)
         {
-            osFlags |= MEM_COMMIT;
-        }
-    }
+            if (flags & HPF_NO_ACCESS)
+            {
+                return PAGE_NOACCESS;
+            }
 
-    unsigned int TransformHPF2PageFlags(unsigned int flags)
-    {
-        if (flags & HPF_NO_ACCESS)
-        {
-            return PAGE_NOACCESS;
-        }
+            if (flags & HPF_EXECUTABLE)
+            {
+                if (flags & HPF_READ && flags & HPF_WRITE)
+                {
+                    return PAGE_EXECUTE_READWRITE;
+                }
 
-        if (flags & HPF_EXECUTABLE)
-        {
+                if (flags & HPF_READ)
+                {
+                    return PAGE_EXECUTE_READ;
+                }
+
+                if (flags & HPF_WRITE)
+                {
+                    return PAGE_EXECUTE_WRITECOPY;
+                }
+            }
+
             if (flags & HPF_READ && flags & HPF_WRITE)
             {
-                return PAGE_EXECUTE_READWRITE;
+                return PAGE_READWRITE;
             }
 
             if (flags & HPF_READ)
             {
-                return PAGE_EXECUTE_READ;
+                return PAGE_READONLY;
             }
 
             if (flags & HPF_WRITE)
             {
-                return PAGE_EXECUTE_WRITECOPY;
+                return PAGE_WRITECOPY;
             }
+
+            return 0;
         }
 
-        if (flags & HPF_READ && flags & HPF_WRITE)
+        void* AllocPage(size_t pages, unsigned int flags)
         {
-            return PAGE_READWRITE;
+
         }
 
-        if (flags & HPF_READ)
+        bool FreePage(void* page)
         {
-            return PAGE_READONLY;
+
         }
 
-        if (flags & HPF_WRITE)
+        bool ModifyPage(void* page, size_t pages, unsigned int flags)
         {
-            return PAGE_WRITECOPY;
+
         }
 
-        return 0;
-    }
-
-    void* AllocPage(size_t pages, unsigned int flags)
-    {
-        
-    }
-
-    bool FreePage(void* page)
-    {
-
-    }
-
-    bool ModifyPage(void* page, size_t pages, unsigned int flags)
-    {
-
-    }
-
-    struct 
-    {
-        bool         bIsCached               = false;
-        unsigned int dwAllocationGranularity = 0;
-        unsigned int dwPageSize              = 0;
-    } OSCache;
-
-    // Cache OS page allocation informations by using GetSystemInfo.
-    void TryCacheOSInformation()
-    {
-        SYSTEM_INFO sysInfo;
-        GetSystemInfo(&sysInfo);
-
-        OSCache.dwPageSize              = sysInfo.dwPageSize;
-        OSCache.dwAllocationGranularity = sysInfo.dwAllocationGranularity;
-    }
-
-    size_t QueryPageSize()
-    {
-        if(!OSCache.bIsCached)
+        struct 
         {
-            TryCacheOSInformation();
-        }
+            bool         bIsCached               = false;
+            unsigned int dwAllocationGranularity = 0;
+            unsigned int dwPageSize              = 0;
+        } OSCache;
 
-        return (size_t)OSCache.dwPageSize;
-    }
-
-    size_t QueryPageGranularity()
-    {
-        if(!OSCache.bIsCached)
+        // Cache OS page allocation informations by using GetSystemInfo.
+        void TryCacheOSInformation()
         {
-            TryCacheOSInformation();
+            SYSTEM_INFO sysInfo;
+            GetSystemInfo(&sysInfo);
+
+            OSCache.dwPageSize              = sysInfo.dwPageSize;
+            OSCache.dwAllocationGranularity = sysInfo.dwAllocationGranularity;
         }
 
-        return (size_t)OSCache.dwAllocationGranularity;
+        size_t QueryPageSize()
+        {
+            if(!OSCache.bIsCached)
+            {
+                TryCacheOSInformation();
+            }
+
+            return (size_t)OSCache.dwPageSize;
+        }
+
+        size_t QueryPageGranularity()
+        {
+            if(!OSCache.bIsCached)
+            {
+                TryCacheOSInformation();
+            }
+
+            return (size_t)OSCache.dwAllocationGranularity;
+        }
     }
-}
+}   
 
 #else
 #error Unsupported Platform
