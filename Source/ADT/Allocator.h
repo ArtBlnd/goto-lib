85f464485879aa3e85d24be7c219ca906a4e93c2
JunRyung Ju <junryoungju@gmail.com>
2019-06-28 10:56:11 +0900

    Implements allocation of SmallAllocatorStorage

--- Source/ADT/Allocator.h
+++ Source/ADT/Allocator.h
@@ -2,6 +2,7 @@
 #define __GTFW_LIB_ADT_ALLOCATOR_H__
 
 #include <type_traits>
+#include <Host/Memory.h>
 
 #ifndef NO_VTABLE
 #ifdef _MSC_VER
@@ -17,6 +18,12 @@ namespace Goto
 {
     namespace ADT
     {
+        template <class T1, class T2>
+        inline T1 OffsetPtr(T1 base, T2 offs)
+        {
+            return (T1)((size_t)(base) + (size_t)(offs));
+        }
+
         NO_VTABLE class IAllocatorStorage
         {
             size_t m_asCapacity;
@@ -42,32 +49,47 @@ namespace Goto
             size_t GetAllocatedCount() const;
         };
 
-        template <size_t SmallSize, size_t PtrSize = sizeof(void*)>
         struct SmallStorageBuffer
         {
             SmallStorageBuffer* m_ssbPriv = nullptr;
+            char                m_ssbSmallBuf[0];
         };
 
         template <size_t SmallSize>
         class SmallAllocatorStroage : IAllocatorStorage
         {
-            size_t StorageLeftBytes = SmallSize;
+            size_t m_sasSotrageBytesLeft = SmallSize;
 
-            union 
+            union
             {
-                char               uStorageBuf[SmallSize];
-                SmallStorageBuffer uStorageSSO;
+                char                StorageBuf[SmallSize + sizeof(void*)];
+                SmallStorageBuffer* StorageSSO;
             }
+            
 
         protected:
             SmallAllocatorStroage() : IAllocatorStorage(SmallSize) { }
 
             void* AllocateMemoryImpl(size_t size)
             {
-                if (StorageLeftBytes >= size)
+                void* newMem = nullptr;
+
+                if (m_sasStorageBytesLeft >= size)
                 {
-                    
+                    m_sasStorageBytesLeft -= size;
+                    newMem = OffsetPtr(uStorageSSO.m_ssbSmallBuf, m_sasStorageBytesLeft);
                 }
+                else
+                {
+                    SmallStorageBuffer* oldStorage = &uStorageSSO;
+                    StorageSSO = (SmallStorageBuffer*) Host::AllocPage(1, HPF_COMMIT | HPF_READ | HPF_WRITE);
+                    StorageSSO->m_ssbPriv = oldStorage;
+
+                    m_sasStorageBytesLeft = Host::QueryPageSize();
+                    return AllocateMemoryImpl(size);
+                }
+
+                return newMem;
             }
         };
 
@@ -88,16 +110,21 @@ namespace Goto
             static_assert(std::is_base_of<IAllocatorStorage, StorageT>::value, "Storage type must be IAllocatorStorage");
 
         public:
-            void* allocate(size_t size);
-            void* allocate(size_t size, size_t align);
+            void* allocate(size_t size)
+            {
+                return AllocateMemoryImpl(size);
+            }
+
+            void* allocate(size_t size, size_t align)
+            {
+                return AllocateMemoryImpl(size);
+            }
 
             template <class T>
             T* allocate()
             {
                 return (T*) allocate(sizeof(T));
             }
-
-
         };
 
     }
